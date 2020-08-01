930b03bce9179ee117c3aa81bfb44296e8313ac2
charlesetiennecorriveau <charlesetiennec@gmail.com>
2020-07-31 22:14:10 -0400

    Added nodemailer and sends best confirmation email ever

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var mysql = require('mysql');
 var shajs = require('sha.js');
 const crypto = require('crypto');
+var nodemailer = require('nodemailer');
+
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'event_planner',
@@ -11,97 +13,127 @@ var connection = mysql.createConnection({
     database: 'event_planner'
 });
 
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'beansplanner@gmail.com',
+        pass: 'U@56Elh9DSQS'
+    }
+});
+
 router.post('/register', [
     body('email').trim()
-      .notEmpty().withMessage("Email can not be empty.")
-      .isEmail().withMessage("Email must be a valid email address.")
-      .normalizeEmail()
-      .escape(),
+        .notEmpty().withMessage("Email can not be empty.")
+        .isEmail().withMessage("Email must be a valid email address.")
+        .normalizeEmail()
+        .escape(),
     body('username').trim()
-      .notEmpty().withMessage('Username can not be empty.')
-      .not().matches(/[^\w-]+/g).withMessage("Username can contain only letters, numbers, '-' and '_'.")
-      .escape(),
+        .notEmpty().withMessage('Username can not be empty.')
+        .not().matches(/[^\w-]+/g).withMessage("Username can contain only letters, numbers, '-' and '_'.")
+        .escape(),
     body('password').trim()
-      .isLength({min: 8}).withMessage('Password must be longer than 8 characters.')
-      .matches(/[$-/:-?{-~!"^_`\[\]]/).withMessage('Password must contain a symbol.')
-      .matches(/[A-Z]/).withMessage('Password must contain an uppercase letter.')
-      .matches(/[a-z]/).withMessage('Password must contain a lowercase letter.')
-      .matches(/\d/).withMessage('Password must contain a digit.')
-      .escape()
-], function(req, res){
+        .isLength({ min: 8 }).withMessage('Password must be longer than 8 characters.')
+        .matches(/[$-/:-?{-~!"^_`\[\]]/).withMessage('Password must contain a symbol.')
+        .matches(/[A-Z]/).withMessage('Password must contain an uppercase letter.')
+        .matches(/[a-z]/).withMessage('Password must contain a lowercase letter.')
+        .matches(/\d/).withMessage('Password must contain a digit.')
+        .escape()
+], function (req, res) {
     const errors = validationResult(req);
 
-    if(!errors.isEmpty()) {
+    if (!errors.isEmpty()) {
         return res.status(422).json({
             success: false, errors: errors.array()
 
         });
     }
 
-    const {username, email, password, fullname} = req.body;
+    const { username, email, password, fullname } = req.body;
     //Check login info available
     connection.query(`SELECT email, username FROM users WHERE email = "${email}" OR username = "${username}"`, (error, results) => {
-        if (!error){
+        if (!error) {
             //Not available
-            if (results.length > 0)
-            {
+            if (results.length > 0) {
                 const usernameTaken = results.filter(row => row.username === username).length;
                 const emailTaken = results.filter(row => row.email === email).length;
-                if (emailTaken || usernameTaken){
+                if (emailTaken || usernameTaken) {
                     const errorStr = `${emailTaken ? "Email" : ""}${emailTaken && usernameTaken ? " and " : ""}${usernameTaken ? "Username" : ""} already in use.`
-                    res.status(422).json({success: false, errors: [{
-                        "value": "",
-                        "msg": errorStr,
-                        "param": `${emailTaken ? "email" : ""}${emailTaken && usernameTaken ? ", " : ""}${usernameTaken? "username" : ""}`,
-                        "location": "body"
-                    }]})
+                    res.status(422).json({
+                        success: false, errors: [{
+                            "value": "",
+                            "msg": errorStr,
+                            "param": `${emailTaken ? "email" : ""}${emailTaken && usernameTaken ? ", " : ""}${usernameTaken ? "username" : ""}`,
+                            "location": "body"
+                        }]
+                    })
                 }
             }
             //Available
             else {
                 //Hash password
                 const salt = makesalt(16);
-                const hash = shajs('sha256').update(password+salt).digest('hex');
+                const hash = shajs('sha256').update(password + salt).digest('hex');
 
                 connection.query(`INSERT INTO users (username,${fullname ? " fullname," : ""} email, password_hash, password_salt) VALUES ("${username}",${fullname ? `"${fullname}", ` : ""} "${email}", "${hash}", "${salt}")`, function (error, results, fields) {
-                    if (!error){
-                        req.session.userId =  results.insertId;
-                        res.status(201).json({success: true});
+                    if (!error) {
+                        req.session.userId = results.insertId;
+                        // Send confirmation email
+
+
+                        var mailOptions = {
+                            from: 'youremail@gmail.com',
+                            to: email,
+                            subject: 'Email Confirmation',
+                            html: '<h1>ASS</h1>'
+                        };
+
+                        transporter.sendMail(mailOptions, function (error, info) {
+                            if (error) {
+                                console.log(error);
+                            } else {
+                                console.log('Email sent: ' + info.response);
+                            }
+                        });
+                        res.status(201).json({ success: true });
                     }
                     else {
-                        res.status(500).json({success: false, errors: [{
-                            "value": "",
-                            "msg": "Could not create user.",
-                            "param": "",
-                            "location": ""
-                        }]})
+                        res.status(500).json({
+                            success: false, errors: [{
+                                "value": "",
+                                "msg": "Could not create user.",
+                                "param": "",
+                                "location": ""
+                            }]
+                        })
                     }
                 });
             }
         }
-        else res.status(500).json({success: false, errors: [{
-            "value": "",
-            "msg": "Unknown server error.",
-            "param": "",
-            "location": ""
-        }]})
+        else res.status(500).json({
+            success: false, errors: [{
+                "value": "",
+                "msg": "Unknown server error.",
+                "param": "",
+                "location": ""
+            }]
+        })
     });
 })
 
 router.post('/login', [
     body('email').trim()
-      .notEmpty().withMessage("Email/Username can not be empty."),
+        .notEmpty().withMessage("Email/Username can not be empty."),
     body('email').if(body('email').isEmail())
-      .normalizeEmail(),
+        .normalizeEmail(),
     body('email')
-      .escape(),
+        .escape(),
     body('password').trim()
-      .notEmpty().withMessage("Password can not be empty.")
-      .escape()
-], function(req, res) {
+        .notEmpty().withMessage("Password can not be empty.")
+        .escape()
+], function (req, res) {
     const errors = validationResult(req);
 
-    if(!errors.isEmpty()) {
+    if (!errors.isEmpty()) {
         return res.status(422).json({
             success: false, errors: errors.array()
 
@@ -111,50 +143,56 @@ router.post('/login', [
     const { email, password } = req.body;
     connection.query(`SELECT * FROM users WHERE email = "${email}" OR username = "${email}"`, (error, results, fields) => {
         if (!error) {
-            if(results.length > 0){
+            if (results.length > 0) {
                 const user = results[0];
-                const hash = shajs('sha256').update(password+user.password_salt).digest('hex');
+                const hash = shajs('sha256').update(password + user.password_salt).digest('hex');
                 console.log(hash)
-                if (hash === user.password_hash){
+                if (hash === user.password_hash) {
                     req.session.userId = user.id;
-                    res.status(201).json({success: true});
+                    res.status(201).json({ success: true });
                 }
                 else
-                    res.status(403).json({success: false, errors: [{
+                    res.status(403).json({
+                        success: false, errors: [{
+                            "value": "",
+                            "msg": "Invalid username or password.",
+                            "param": "username,password",
+                            "location": "body"
+                        }]
+                    });
+            }
+            else {
+                res.status(403).json({
+                    success: false, errors: [{
                         "value": "",
                         "msg": "Invalid username or password.",
                         "param": "username,password",
                         "location": "body"
-                    }]});
-            }
-            else {
-                res.status(403).json({success: false, errors: [{
-                    "value": "",
-                    "msg": "Invalid username or password.",
-                    "param": "username,password",
-                    "location": "body"
-                }]});
+                    }]
+                });
             }
         }
     });
 })
 
-router.post('/logout', function(req, res) {
+router.post('/logout', function (req, res) {
     req.session.destroy(err => {
         res.clearCookie('sid');
         if (err)
-            res.status(401).json({success: false, errors: [{
-                "value": "",
-                "msg": "No session found.",
-                "param": "",
-                "location": "cookies"
-            }]});
+            res.status(401).json({
+                success: false, errors: [{
+                    "value": "",
+                    "msg": "No session found.",
+                    "param": "",
+                    "location": "cookies"
+                }]
+            });
         else
-            res.status(201).json({success: true});
+            res.status(201).json({ success: true });
     })
 })
 
-router.post('/confirmation', function(req, res) {
+router.post('/confirmation', function (req, res) {
 
 })
 
