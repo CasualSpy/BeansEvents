7841a048325a4e2169fb490a4be9d691f4e823e6
CasualSpy <arnaudbegin@hotmail.com>
2020-08-17 02:35:25 -0400

    I want to die

--- routes/events.js
+++ routes/events.js
@@ -8,6 +8,7 @@ var connection = mysql.createConnection({
     database: 'event_planner'
 })
 const {body, validationResult} = require('express-validator');
+const collect = require('collect.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -63,4 +64,37 @@ router.post('/create', [
     });
 });
 
+router.get('/invited', function (req, res) {
+    const user = req.session.userId;
+    connection.query(`SELECT e.id AS event_id, e.title, e.location, e.start_time, s.fullname AS sender_fullname, s.username AS sender_username FROM events e LEFT JOIN invitations i ON e.id = i.event_id RIGHT JOIN users u ON i.receiver_id = u.id INNER JOIN users s ON s.id = i.sender_id WHERE u.id = ${user}`)
+})
+
+router.get('/going', function (req, res) {
+    const user = req.session.userId;
+    const query = `SELECT e.id AS event_id, e.title, e.location, e.start_time, i.id AS invitation_id, s.id AS sender_id, s.username AS sender_username, s.fullname AS sender_fullname, fr.friend_id, fr.username AS friend_username, fr.fullname AS friend_fullname FROM events e INNER JOIN invitations i ON e.id = i.event_id INNER JOIN users s ON s.id = i.sender_id INNER JOIN users u ON u.id = i.receiver_id LEFT JOIN (SELECT f.user2_id AS friend_id, fu.username, fu.fullname, r.event_id FROM friends f INNER JOIN responses r ON r.user_id = f.user2_id INNER JOIN users u ON u.id = f.user1_id INNER JOIN users fu ON fu.id = f.user2_id WHERE u.id = ${user}) fr ON fr.event_id = i.event_id WHERE u.id = ${user};`
+    connection.query(query, function (error, results) {
+        if (!error) {
+            const temp = collect(results).groupBy('invitation_id')
+            let ret = []
+            for (invitation in temp.all()){
+                const {event_id, title, location, start_time, invitation_id, sender_id, sender_username, sender_fullname} = temp.all()[invitation].items[0]
+                const friends = temp.all()[invitation].items.map(item => ({id: item.friend_id, username: item.friend_username, fullname: item.friend_fullname}))
+                ret.push({event_id, title, location, start_time, invitation_id, sender_id, sender_username, sender_fullname, friends})
+            }
+
+            res.status(200).json(ret)
+        }
+        else {
+            console.log(query)
+            res.status(500).json({
+                success: false, errors: [{
+                    "value": "",
+                    "msg": "Unknown server error.",
+                    "param": "",
+                    "location": ""
+                }]
+            })
+        }
+    })
+})
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -38,6 +38,8 @@ router.post('/register', [
         .matches(/[A-Z]/).withMessage('Password must contain an uppercase letter.')
         .matches(/[a-z]/).withMessage('Password must contain a lowercase letter.')
         .matches(/\d/).withMessage('Password must contain a digit.')
+        .escape(),
+    body('fullname').trim()
         .escape()
 ], function (req, res) {
     const errors = validationResult(req);
@@ -232,72 +234,107 @@ router.post('/confirmation', [
 
 router.post('/resend_email', function (req, res) {
     const user = req.session.userId;
-    connection.query(`SELECT email, verif_token FROM users WHERE id = ${user}`, function (error, results) {
-        if (!error) {
-            const email = results[0].email;
-            const token = results[0].verif_token;
-            var mailOptions = {
-                from: 'beansplanner@gmail.com',
-                to: email,
-                subject: 'Email Confirmation',
-                html: `<div>
-                            <h1>Please confirm your email address by clicking on the button below</h1>
-                            http://localhost:3000/email_confirmation/${token}
-                        </div>`
-            };
+    if (user){
+        connection.query(`SELECT email, verif_token FROM users WHERE id = ${user}`, function (error, results) {
+            if (!error) {
+                const email = results[0].email;
+                const token = results[0].verif_token;
+                var mailOptions = {
+                    from: 'beansplanner@gmail.com',
+                    to: email,
+                    subject: 'Email Confirmation',
+                    html: `<div>
+                                <h1>Please confirm your email address by clicking on the button below</h1>
+                                http://localhost:3000/email_confirmation/${token}
+                            </div>`
+                };
 
-            transporter.sendMail(mailOptions, function (error, info) {
-                if (error) {
-                    console.log(error);
-                } else {
-                    console.log('Email sent: ' + info.response);
-                }
-            });
-            res.status(201).json({success: true})
-        } else {
-            res.status(500).json({
-                success: false, errors: [{
-                    "value": "",
-                    "msg": "Unknown server error.",
-                    "param": "",
-                    "location": ""
-                }]
-            })
-        }
-    })
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
+                });
+                res.status(201).json({success: true})
+            } else {
+                res.status(500).json({
+                    success: false, errors: [{
+                        "value": "",
+                        "msg": "Unknown server error.",
+                        "param": "",
+                        "location": ""
+                    }]
+                })
+            }
+        })
+    }
+    else res.status(401).json({success:false, errors:[{
+        "value": "",
+        "msg": "User is not logged in.",
+        "param": "",
+        "location": ""
+    }]})
 })
 
 router.get('/user/:username', function (req, res) {
     const username = req.params.username;
     if (username) {
-        connection.query(
-            `SELECT u.username, u.fullname, e.id AS event_id, e.title, e.location, e.start_time FROM events e LEFT JOIN responses r ON e.id = r.event_id RIGHT JOIN users u ON e.created_by = u.id OR r.user_id = u.id WHERE u.username = "${username}";`, function (error, results) {
-                if (!error){
-                    if (results.length > 0) {
-                        const {username, fullname} = results[0];
-                        const events = results.map(r => ({id: r.event_id, title: r.title, location: r.location, start_time: r.start_time}));
-                        res.status(200).json({success:true, username, fullname, events: (events[0].id ? events : [])});
-                    }
-                    else res.status(404).json({
-                        success: false, errors: [{
-                            "value": username,
-                            "msg": "User not found.",
-                            "param": "username",
-                            "location": "params"
-                        }]
-                    })
+        connection.query(`SELECT u.username, u.fullname, e.id AS event_id, e.title, e.location, e.start_time FROM events e LEFT JOIN responses r ON e.id = r.event_id RIGHT JOIN users u ON e.created_by = u.id OR r.user_id = u.id WHERE u.username = "${username} AND e.is_private = FALSE";`, function (error, results) {
+            if (!error){
+                if (results.length > 0) {
+                    const {username, fullname} = results[0];
+                    const events = results.map(r => ({id: r.event_id, title: r.title, location: r.location, start_time: r.start_time}));
+                    res.status(200).json({success:true, username, fullname, events: (events[0].id ? events : [])});
                 }
-                else {
-                    res.status(500).json({
-                        success: false, errors: [{
-                            "value": "",
-                            "msg": "Unknown server error.",
-                            "param": "",
-                            "location": ""
-                        }]
-                    })
-                }
-            });
+                else res.status(404).json({
+                    success: false, errors: [{
+                        "value": username,
+                        "msg": "User not found.",
+                        "param": "username",
+                        "location": "params"
+                    }]
+                })
+            }
+            else {
+                res.status(500).json({
+                    success: false, errors: [{
+                        "value": "",
+                        "msg": "Unknown server error.",
+                        "param": "",
+                        "location": ""
+                    }]
+                })
+            }
+        });
+    }
+})
+
+router.get('/details', function (req, res) {
+    const user = req.session.userId;
+    if (user) {
+        connection.query(`SELECT username, fullname FROM users WHERE id = ${user}`, function (error, results) {
+            if (!error){
+            }
+            else {
+                res.status(500).json({
+                    success: false, errors: [{
+                        "value": "",
+                        "msg": "Unknown server error.",
+                        "param": "",
+                        "location": ""
+                    }]
+                })
+            }
+        })
+    }
+    else {
+        res.status(401).json({success: false, errors: [{
+            "value": "",
+            "msg": "User is not logged in.",
+            "param": "",
+            "location": ""
+        }]})
     }
 })
 
